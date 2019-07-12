540b0957e7feb7220958afca5e7b562b9f8d85e0
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-07-12 19:24:10 +0100

    Add link to Palette

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends Component {
 	        	<Route 
 	        		exact 
 	        		path="/"
-	        		render={() => (
-	        			<PaletteList palettes={seedColors} />
+	        		render={(routeProps) => (
+	        			<PaletteList palettes={seedColors} {...routeProps} />
 	        		)} 
 	        	/>
 	        	<Route 
